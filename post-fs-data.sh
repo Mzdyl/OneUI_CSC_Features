f856aa9d9a8efd6773130ed7eeb734dbb066e5f3
Mzdyl <Mzdyl@MacBook-Air.lan>
2023-04-09 01:37:59 +0800

    vendor

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -6,10 +6,10 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 mount --bind $MODDIR/optics /optics
-mkdir -p $MODDIR/system/vendor/etc
-cp -af /vendor/etc/floating_feature.xml $MODDIR/system/vendor/etc
-
-# BixbyTouch
-sed -i '/<\/SecFloatingFeatureSet>/i\<SEC_FLOATING_FEATURE_COMMON_SUPPORT_BIXBY_TOUCH>TRUE<\/SEC_FLOATING_FEATURE_COMMON_SUPPORT_BIXBY_TOUCH>' $MODDIR/system/vendor/etc/floating_feature.xml
-# Auto Power on and off
-sed -i '/<\/SecFloatingFeatureSet>/i\<SEC_FLOATING_FEATURE_SETTINGS_SUPPORT_AUTO_POWER_ON_OFF>TRUE<\/SEC_FLOATING_FEATURE_SETTINGS_SUPPORT_AUTO_POWER_ON_OFF>' $MODDIR/system/vendor/etc/floating_feature.xml
+#mkdir -p $MODDIR/system/vendor/etc
+#cp -af /vendor/etc/floating_feature.xml $MODDIR/system/vendor/etc
+#
+## BixbyTouch
+#sed -i '/<\/SecFloatingFeatureSet>/i\<SEC_FLOATING_FEATURE_COMMON_SUPPORT_BIXBY_TOUCH>TRUE<\/SEC_FLOATING_FEATURE_COMMON_SUPPORT_BIXBY_TOUCH>' $MODDIR/system/vendor/etc/floating_feature.xml
+## Auto Power on and off
+#sed -i '/<\/SecFloatingFeatureSet>/i\<SEC_FLOATING_FEATURE_SETTINGS_SUPPORT_AUTO_POWER_ON_OFF>TRUE<\/SEC_FLOATING_FEATURE_SETTINGS_SUPPORT_AUTO_POWER_ON_OFF>' $MODDIR/system/vendor/etc/floating_feature.xml
