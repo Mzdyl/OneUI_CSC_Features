1aadf2f3616eb3f1af0af7374946417383234d6f
Mzdyl <25149539+Mzdyl@users.noreply.github.com>
2023-11-03 21:04:50 +0800

    更新

--- module_files/customize.sh
+++ module_files/customize.sh
@@ -19,21 +19,7 @@
 # IS64BIT(bool)：如果$ARCH是arm64或者x64则值为true
 # API(int)：设备的 API 级别（Android 版本）（例如21，对于 Android 5.0）
 
-# 监听音量键
-Volume_key_monitoring() {
-	local choose
-	local branch
-	while :; do
-		choose="$(getevent -qlc 1 | awk '{ print $3 }')"
-		case "$choose" in
-			KEY_VOLUMEUP) branch="0" ;;
-			KEY_VOLUMEDOWN) branch="1" ;;
-			*) continue ;;
-		esac
-		echo "$branch"
-		break
-	done
-}
+
 
 # 若要启用 Magic Mount，请将其设置为 true；若不启用，请设置为 false
 # 大多数模块都需要启用它
@@ -65,44 +51,22 @@ ui_print "- 你的设备地区CSC是 $CSC 尝试自动适配"
 CSC_DIR=$(find /optics/configs/carriers -type d -name "$CSC")
 mv $MODPATH/optics/configs/carriers/TGY $MODPATH/$CSC_DIR
 
-ui_print "—————————————————————————————————————"
-ui_print "- 按音量键＋: 安装全功能版（有BUG,待修复）"
-ui_print "- 按音量键－: 安装精简功能版（无BUG，应该）"
-ui_print "—————————————————————————————————————"
-sleep 0.07
+ui_print "开始安装"
+sleep 0.5
+REPLACE="
+/system/app/MinusOnePage
+/system/priv-app/Firewall
+"
 
-if [[ $(Volume_key_monitoring) == 0 ]]; then
-	ui_print "全功能版开始安装"
-	REPLACE="
-	/system/priv-app/ShareLive
-	/system/app/AllShareAware
-	/system/app/MdxKitService
-	"
-	
-else
-	ui_print "精简功能版开始安装"
-	sleep 0.5
-	REPLACE="
-	/system/app/MinusOnePage
-	/system/priv-app/Firewall
-	"
-	rm -rf "$MODPATH/system/priv-app/AppLock"
-	rm -rf "$MODPATH/system/priv-app/ShareLive"
-	rm -rf "$MODPATH/system/etc/default-permissions"
-	rm -rf "$MODPATH/system/etc/sysconfig/bixbytouchapp.xml"
-	rm -rf "$MODPATH/system/etc/permissions/privapp-permissions-com.samsung.android.app.sharelive.xml"
-	rm -rf "$MODPATH/system/etc/permissions/privapp-permissions-com.samsung.android.applock.xml"
-	rm -rf "$MODPATH/system/etc/permissions/privapp-permissions-com.samsung.android.mdx.xml"
-	rm -rf "$MODPATH/system/app/AllShareAware"
-	rm -rf "$MODPATH/system/app/ChinaHiddenMenu"
-	rm -rf "$MODPATH/system/app/ChnFileShareKitService"
-	rm -rf "$MODPATH/system/app/MdxKitService"
-fi
+ui_print "移植 Fresh 系统相关功能代码"
 
 ui_print "添加 切换至使用更好的 WLAN 网络"
-settings put global sem_wifi_switch_to_better_wifi_supported 1
 ui_print "添加 关闭 ADB 安装验证"
-settings put global verifier_verify_adb_installs 0
+ui_print "添加 禁用锁帧"
+ui_print "添加 强制全局240Hz采样率"
+
+ui_print "软件增删改详见 README.md"
+
 
 set_perm_recursive  $MODPATH  0  0  0777  0777
 
