d994f8524c78cf36e6ea282e988d4e6bbf1f9461
Mzdyl <25149539+Mzdyl@users.noreply.github.com>
2026-02-25 06:37:57 +0800

    Remove Old

--- scr/auto_modify_cscfeature/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := auto_modify_cscfeature
-LOCAL_SRC_FILES := auto_modify_cscfeature.c
-
-include $(BUILD_EXECUTABLE)
--- scr/auto_modify_json_csc/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := auto_modify_jsoncsc
-LOCAL_SRC_FILES := auto_modify_jsoncsc.c
-
-include $(BUILD_EXECUTABLE)
